5ee3e4a974935da133090f772ac46e64ec058ab5
Egor fureunoir Gorbunov <contact@fureunoir.com>
2025-05-19 17:20:28 +0300

    Features: 1) Add 'country' field to geo model;
    Fixes: 1) Remove redundant fields 'location', 'raw_data', 'api_response', and 'user' from 'address' model;
    
    Extra: Adjust formatting by removing an unnecessary blank line;

--- geo/migrations/0004_add_missing_fields.py
+++ geo/migrations/0004_add_missing_fields.py
@@ -1,6 +1,3 @@
-import django.contrib.gis.db.models.fields
-from django.conf import settings
-
 from django.db import migrations, models
 
 
@@ -29,29 +26,5 @@ class Migration(migrations.Migration):
             model_name='address',
             name='country',
             field=models.CharField(max_length=255, null=True, verbose_name='country'),
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='location',
-            field=django.contrib.gis.db.models.PointField(blank=True, geography=True,
-                                                          help_text='Geolocation point: (longitude, latitude)',
-                                                          null=True, srid=4326),
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='raw_data',
-            field=models.JSONField(blank=True, help_text='Full JSON response from geocoder for this address',
-                                   null=True),
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='api_response',
-            field=models.JSONField(blank=True, help_text='Stored JSON response from the geocoding service', null=True),
-        ),
-        migrations.AddField(
-            model_name='address',
-            name='user',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.CASCADE,
-                                    to=settings.AUTH_USER_MODEL),
         ),
     ]
